e8f7dde06d2913346fa600e1ad5e8903d636334b
Jo Brunner <jo.brunner@mayflower.de>
2016-12-29 17:31:57 +0100

    Adds an initial xcode project

---
+++ FeedbackCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeedbackCards
+//
+//  Created by Jo Brunner on 29.12.16.
+//  Copyright © 2016 Mayflower GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
